b9c2901210177fc4c839c89e0f3cbabda4fadb64
hadeermoustafa <hadeer.moustafa@outlook.com>
2024-03-31 23:27:29 +0200

    Update if_even_multiply_by_eight_or_if_odd_by_bibe.js

--- if_even_multiply_by_eight_or_if_odd_by_bibe.js
+++ if_even_multiply_by_eight_or_if_odd_by_bibe.js
@@ -1,6 +1,5 @@
 //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 function simpleMultiplication(number) {
     // your code........
-  number % 2 == 0? number*= 8 : number*= 9;
-  return number;
+  return % 2 == 0? number*= 8 : number*= 9;
 }
